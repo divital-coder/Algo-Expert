5b81b728f0b0dfb52bb18b7b79813c3b39d53781
Divyansh Goyal <divital2004@gmail.com>
2023-01-15 02:03:51 +0530

    working out

---
+++ file.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main(){
+ 	char name[100];
+	cout << "hi please provide with relevant information when asked :)\n";
+	cout << "please enter your name mate : ";
+	cin  >> name ;
+	cout << "welcome "<< name;
+	return 0;
+}
+
